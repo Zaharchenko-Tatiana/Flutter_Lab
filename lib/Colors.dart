beda0da5acca372f06504b5a57855f5deb9dcd11
Захарченко Татьяна <zaharchenkote@gmail.com>
2023-12-05 22:32:47 +0600

    Added header and colors

---
+++ lib/Colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+
+class Colors_add {
+  static const ConstantColor =  Color(0xFF08A652);
+  static const Text_unpressed =  Color(0x8C000000);
+  static const Fon_button =  Color(0x14000000);
+}
