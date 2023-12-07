4d7b558935bbc101d15349d650fe8c7a75b5d8ec
Захарченко Татьяна <zaharchenkote@gmail.com>
2023-12-07 17:45:31 +0600

    Add scroll horizontal, vertical snd subscriptions updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/sub_scroll.dart';
 import 'package:flutter_app/subscriptions.dart';
 import 'package:flutter_app/tabs.dart';
 import 'Person.dart';
@@ -10,45 +11,44 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
-        appBar: AppBar(
+          appBar: AppBar(
             actions: <Widget>[
               Container(
                 margin: const EdgeInsets.symmetric(horizontal: 14.0),
-        ),
-              IconButton(
-                icon: const Icon(Icons.clear, color: Colors_add.ConstantColor, size: 24),
-               onPressed: () {  },
               ),
-
+              IconButton(
+                icon: const Icon(Icons.clear,
+                    color: Colors_add.ConstantColor, size: 24),
+                onPressed: () {},
+              ),
               const Spacer(),
-
               IconButton(
                   icon: const Icon(Icons.exit_to_app, size: 24),
-                  onPressed: () {
-                  },
-                  color: Colors_add.ConstantColor
-              ),
+                  onPressed: () {},
+                  color: Colors_add.ConstantColor),
               Container(
                 margin: const EdgeInsets.symmetric(horizontal: 14.0),
               ),
             ],
           ),
-
-        body: Column(
-          children: [
-            person(),
-            const SizedBox(height: 14),
-            tabs(),
-            subscriptions(),
-
-    ],
-    ),
-    )
-    );
+          body: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: Column(
+              children: [
+                person(),
+                const SizedBox(height: 14),
+                tabs(),
+                subscriptions(),
+                subScroll(),
+              ],
+            ),
+          ),
+        ));
   }
 }
---
+++ lib/sub_scroll.dart
@@ -0,0 +1,183 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'Colors.dart';
+
+Widget subScroll() {
+  return Column(
+    children: [
+      Container(
+        color: Colors_add.Fon_befor_tabs,
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          padding: const EdgeInsets.all(14.0),
+          child: Row(
+            children: [
+              Container(
+                width: 216,
+                height: 130,
+                clipBehavior: Clip.antiAlias,
+                decoration: ShapeDecoration(
+                  color: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    const SizedBox(height: 14),
+                    Row(
+                      children: [
+                        const SizedBox(width: 16),
+                        Container(
+                          width: 36,
+                          height: 36,
+                          decoration: ShapeDecoration(
+                            image: const DecorationImage(
+                              image: AssetImage('res/sber_1.jpg'),
+                              fit: BoxFit.fill,
+                            ),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: 12),
+                        const Text(
+                          'СберПрайм',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                            fontFamily: 'SF Pro Text',
+                            fontWeight: FontWeight.w500,
+                            height: 0.08,
+                            letterSpacing: -0.40,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 24),
+                    const Row(
+                      children: [
+                        SizedBox(width: 16),
+                        Text(
+                          'Платёж 9 июля',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 14,
+                            fontFamily: 'SF Pro Text',
+                            fontWeight: FontWeight.w500,
+                            height: 0.09,
+                            letterSpacing: -0.41,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 16),
+                    const Row(
+                      children: [
+                        SizedBox(width: 16),
+                        Text(
+                          '199 ₽ в месяц',
+                          style: TextStyle(
+                            color: Colors_add.Text_unpressed,
+                            fontSize: 14,
+                            fontFamily: 'SF Pro Text',
+                            fontWeight: FontWeight.w500,
+                            height: 0.09,
+                            letterSpacing: -0.41,
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(width: 8),
+              Container(
+                width: 216,
+                height: 130,
+                clipBehavior: Clip.antiAlias,
+                decoration: ShapeDecoration(
+                  color: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    const SizedBox(height: 14),
+                    Row(
+                      children: [
+                        const SizedBox(width: 16),
+                        Container(
+                          width: 36,
+                          height: 36,
+                          decoration: ShapeDecoration(
+                            image: const DecorationImage(
+                              image: AssetImage('res/sber_2.jpg'),
+                              fit: BoxFit.fill,
+                            ),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: 12),
+                        const Text(
+                          'Переводы',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                            fontFamily: 'SF Pro Text',
+                            fontWeight: FontWeight.w500,
+                            height: 0.08,
+                            letterSpacing: -0.40,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 24),
+                    const Row(
+                      children: [
+                        SizedBox(width: 16),
+                        Text(
+                          'Автопродление 21 августа',
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 14,
+                            fontFamily: 'SF Pro Text',
+                            fontWeight: FontWeight.w500,
+                            height: 0.09,
+                            letterSpacing: -0.41,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 16),
+                    const Row(
+                      children: [
+                        SizedBox(width: 16),
+                        Text(
+                          '199 ₽ в месяц',
+                          style: TextStyle(
+                            color: Colors_add.Text_unpressed,
+                            fontSize: 14,
+                            fontFamily: 'SF Pro Text',
+                            fontWeight: FontWeight.w500,
+                            height: 0.09,
+                            letterSpacing: -0.41,
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    ],
+  );
+}
